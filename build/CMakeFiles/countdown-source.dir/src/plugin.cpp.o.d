9755d1a2911a80886d1a8aece7fd854b3d1b762f
Hoopsy <hoopsymakes@proton.me>
2026-05-14 15:39:32 +0300

    very big thing (basically release)

---
+++ build/CMakeFiles/countdown-source.dir/src/plugin.cpp.o.d
@@ -0,0 +1,175 @@
+CMakeFiles/countdown-source.dir/src/plugin.cpp.o: \
+ /home/Hoopsy/Projects/OBS/Countdown/src/plugin.cpp \
+ /usr/include/stdc-predef.h /usr/include/c++/16.1.1/ctime \
+ /usr/include/c++/16.1.1/x86_64-pc-linux-gnu/bits/c++config.h \
+ /usr/include/c++/16.1.1/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/16.1.1/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/include/c++/16.1.1/pstl/pstl_config.h /usr/include/time.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stddef.h \
+ /usr/include/bits/time.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/timex.h /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/struct_timespec.h /usr/include/bits/endian.h \
+ /usr/include/bits/endianness.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/types/struct_itimerspec.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/obs/obs-data.h /usr/include/obs/util/c99defs.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/wchar.h /usr/include/bits/stdint-intn.h \
+ /usr/include/bits/stdint-uintn.h /usr/include/bits/stdint-least.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stdbool.h \
+ /usr/include/sys/types.h /usr/include/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/uintn-identity.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/types/sigset_t.h /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h \
+ /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/obs/media-io/frame-rate.h \
+ /usr/include/obs/obs-module.h /usr/include/obs/obs.h \
+ /usr/include/obs/util/bmem.h /usr/include/obs/util/base.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stdarg.h \
+ /usr/include/wchar.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h /usr/include/bits/types/wint_t.h \
+ /usr/include/bits/types/mbstate_t.h \
+ /usr/include/bits/types/__mbstate_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/string.h \
+ /usr/include/strings.h /usr/include/obs/util/profiler.h \
+ /usr/include/obs/util/darray.h /usr/include/c++/16.1.1/stdlib.h \
+ /usr/include/c++/16.1.1/cstdlib /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-float.h \
+ /usr/include/c++/16.1.1/bits/std_abs.h /usr/include/assert.h \
+ /usr/include/obs/util/text-lookup.h /usr/include/obs/graphics/graphics.h \
+ /usr/include/obs/graphics/input.h /usr/include/obs/graphics/vec2.h \
+ /usr/include/c++/16.1.1/math.h /usr/include/c++/16.1.1/cmath \
+ /usr/include/c++/16.1.1/bits/requires_hosted.h \
+ /usr/include/c++/16.1.1/bits/cpp_type_traits.h \
+ /usr/include/c++/16.1.1/bits/version.h \
+ /usr/include/c++/16.1.1/type_traits \
+ /usr/include/c++/16.1.1/ext/type_traits.h /usr/include/math.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h /usr/include/bits/mathcalls-macros.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
+ /usr/include/bits/iscanonical.h /usr/include/c++/16.1.1/bits/specfun.h \
+ /usr/include/c++/16.1.1/bits/stdexcept_throw.h \
+ /usr/include/c++/16.1.1/bits/exception_defines.h \
+ /usr/include/c++/16.1.1/bits/stdexcept_throwfwd.h \
+ /usr/include/c++/16.1.1/bits/stl_algobase.h \
+ /usr/include/c++/16.1.1/ext/numeric_traits.h \
+ /usr/include/c++/16.1.1/bits/stl_pair.h \
+ /usr/include/c++/16.1.1/bits/move.h \
+ /usr/include/c++/16.1.1/bits/utility.h \
+ /usr/include/c++/16.1.1/bits/stl_iterator_base_types.h \
+ /usr/include/c++/16.1.1/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/16.1.1/bits/concept_check.h \
+ /usr/include/c++/16.1.1/debug/assertions.h \
+ /usr/include/c++/16.1.1/bits/stl_iterator.h \
+ /usr/include/c++/16.1.1/bits/ptr_traits.h \
+ /usr/include/c++/16.1.1/debug/debug.h \
+ /usr/include/c++/16.1.1/bits/predefined_ops.h \
+ /usr/include/c++/16.1.1/bits/stl_function.h \
+ /usr/include/c++/16.1.1/backward/binders.h /usr/include/c++/16.1.1/bit \
+ /usr/include/c++/16.1.1/concepts /usr/include/c++/16.1.1/limits \
+ /usr/include/c++/16.1.1/tr1/gamma.tcc \
+ /usr/include/c++/16.1.1/tr1/special_function_util.h \
+ /usr/include/c++/16.1.1/tr1/bessel_function.tcc \
+ /usr/include/c++/16.1.1/tr1/beta_function.tcc \
+ /usr/include/c++/16.1.1/tr1/ell_integral.tcc \
+ /usr/include/c++/16.1.1/tr1/exp_integral.tcc \
+ /usr/include/c++/16.1.1/tr1/hypergeometric.tcc \
+ /usr/include/c++/16.1.1/tr1/legendre_function.tcc \
+ /usr/include/c++/16.1.1/tr1/modified_bessel_func.tcc \
+ /usr/include/c++/16.1.1/tr1/poly_hermite.tcc \
+ /usr/include/c++/16.1.1/tr1/poly_laguerre.tcc \
+ /usr/include/c++/16.1.1/tr1/riemann_zeta.tcc \
+ /usr/include/obs/graphics/vec3.h /usr/include/obs/graphics/math-defs.h \
+ /usr/include/obs/graphics/vec4.h /usr/include/obs/graphics/srgb.h \
+ /usr/include/obs/util/sse-intrin.h /usr/include/simde/x86/sse2.h \
+ /usr/include/simde/x86/sse.h /usr/include/simde/x86/mmx.h \
+ /usr/include/simde/simde-common.h /usr/include/simde/hedley.h \
+ /usr/include/simde/simde-detect-clang.h /usr/include/simde/simde-arch.h \
+ /usr/include/simde/simde-features.h \
+ /usr/include/simde/simde-diagnostic.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/mm_malloc.h \
+ /usr/include/simde/simde-math.h /usr/include/simde/simde-constify.h \
+ /usr/include/simde/simde-align.h /usr/include/c++/16.1.1/fenv.h \
+ /usr/include/fenv.h /usr/include/bits/fenv.h \
+ /usr/include/c++/16.1.1/cfenv /usr/include/simde/check.h \
+ /usr/include/stdio.h /usr/include/bits/types/__fpos_t.h \
+ /usr/include/bits/types/__fpos64_t.h \
+ /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/simde/debug-trap.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/limits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/pthread_stack_min-dynamic.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/include/bits/uio_lim.h /usr/include/simde/simde-f16.h \
+ /usr/include/simde/simde-common.h /usr/include/obs/media-io/audio-io.h \
+ /usr/include/obs/media-io/media-io-defs.h \
+ /usr/include/obs/util/util_uint64.h /usr/include/obs/media-io/video-io.h \
+ /usr/include/obs/callback/signal.h /usr/include/obs/callback/calldata.h \
+ /usr/include/obs/callback/proc.h /usr/include/obs/obs-config.h \
+ /usr/include/obs/obsconfig.h /usr/include/obs/obs-defs.h \
+ /usr/include/obs/obs-properties.h /usr/include/obs/obs-interaction.h \
+ /usr/include/obs/obs-missing-files.h /usr/include/obs/obs-source.h \
+ /usr/include/obs/obs-encoder.h /usr/include/obs/obs-output.h \
+ /usr/include/obs/obs-service.h /usr/include/obs/obs-audio-controls.h \
+ /usr/include/obs/obs-hotkey.h /usr/include/obs/obs-hotkeys.h \
+ /usr/include/c++/16.1.1/string /usr/include/c++/16.1.1/bits/stringfwd.h \
+ /usr/include/c++/16.1.1/bits/memoryfwd.h \
+ /usr/include/c++/16.1.1/bits/char_traits.h \
+ /usr/include/c++/16.1.1/bits/postypes.h /usr/include/c++/16.1.1/cwchar \
+ /usr/include/c++/16.1.1/bits/allocator.h \
+ /usr/include/c++/16.1.1/x86_64-pc-linux-gnu/bits/c++allocator.h \
+ /usr/include/c++/16.1.1/bits/new_allocator.h /usr/include/c++/16.1.1/new \
+ /usr/include/c++/16.1.1/bits/exception.h \
+ /usr/include/c++/16.1.1/bits/new_except.h \
+ /usr/include/c++/16.1.1/bits/new_throw.h \
+ /usr/include/c++/16.1.1/bits/localefwd.h \
+ /usr/include/c++/16.1.1/x86_64-pc-linux-gnu/bits/c++locale.h \
+ /usr/include/c++/16.1.1/clocale /usr/include/locale.h \
+ /usr/include/bits/locale.h /usr/include/c++/16.1.1/iosfwd \
+ /usr/include/c++/16.1.1/cctype /usr/include/ctype.h \
+ /usr/include/c++/16.1.1/bits/ostream_insert.h \
+ /usr/include/c++/16.1.1/bits/cxxabi_forced.h \
+ /usr/include/c++/16.1.1/bits/range_access.h \
+ /usr/include/c++/16.1.1/initializer_list \
+ /usr/include/c++/16.1.1/bits/erase_if.h \
+ /usr/include/c++/16.1.1/bits/basic_string.h \
+ /usr/include/c++/16.1.1/ext/alloc_traits.h \
+ /usr/include/c++/16.1.1/bits/alloc_traits.h \
+ /usr/include/c++/16.1.1/bits/stl_construct.h \
+ /usr/include/c++/16.1.1/string_view \
+ /usr/include/c++/16.1.1/bits/functional_hash.h \
+ /usr/include/c++/16.1.1/bits/hash_bytes.h \
+ /usr/include/c++/16.1.1/bits/string_view.tcc \
+ /usr/include/c++/16.1.1/ext/string_conversions.h \
+ /usr/include/c++/16.1.1/cstdio /usr/include/c++/16.1.1/cerrno \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/bits/types/error_t.h \
+ /usr/include/c++/16.1.1/bits/charconv.h \
+ /usr/include/c++/16.1.1/bits/basic_string.tcc \
+ /usr/include/c++/16.1.1/bits/memory_resource.h \
+ /usr/include/c++/16.1.1/cstddef \
+ /usr/include/c++/16.1.1/bits/uses_allocator.h \
+ /usr/include/c++/16.1.1/bits/uses_allocator_args.h \
+ /usr/include/c++/16.1.1/tuple /usr/include/c++/16.1.1/bits/invoke.h
